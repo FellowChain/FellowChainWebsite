3cf58c0ef777238486391c04da2189c70781fdeb
Adam skrodzki <adamskrodzki@gmail.com>
2018-06-02 21:16:43 +0200

    pages content edit

--- web/src/components/About.vue
+++ web/src/components/About.vue
@@ -2,8 +2,18 @@
 <template>
   <div class="about">
     <h1>About</h1>
-    <h2>FellowChain Project</h2>
-
+    <p>FellowChain Project is open source (available on github) community based project which aims to build
+    DAO (Decentralized Autonomus Organisation), allowing person without technical knowledge to create own
+  community based insurance cooperative. It aims to implement all necessary logic on blockchain to minimalise bureaucratic
+overhead needed to run such organisation. Use of blockchain will also minimalise trust needed to run such organisation and maximise
+it's transparency and fraud resistance</p>
+<p>
+  FellowChain Project aims to be as blockchain agnostic as possible. Starting its support from Ethereum Rinkeby and POA Network Sokol Network.
+  When project will achieve betatests maturity we will introduce 2nd layer solution based on Ethereum technology.
+</p>
+<p>
+  If You like the idea, You can partiicipate on github, or join us on facebook or telegram (see footer below).
+</p>
   </div>
 </template>
 
--- web/src/components/Footer.vue
+++ web/src/components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
-<b>Footer!</b>
+<span>
+  <b>Join Us!</b>
+<a href="https://t.me/FellowChain"><img src="../assets/telegram.png"  width="18em" height="18em" ></a>
+<a href="https://www.facebook.com/groups/felllowchain/"><img src="../assets/facebook.png"  width="18em" height="18em"></a>
+</span>
 </template>
 <script>
 export default {
