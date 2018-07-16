7d1dad2d38f396eb0dabddba40909480cdc26e53
Adam <adamskrodzki@gmail.com>
2018-07-16 23:04:23 +0200

    industry Needs

--- web/src/components/pages/learn/sub/industryNeeds.vue
+++ web/src/components/pages/learn/sub/industryNeeds.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="content-box">
     <div class="desc">
-      <h3>All You need to know about mutual Insurance</h3>
+      <h3>All You need to know about Token Model!</h3>
+      <h4>Conflict of interests</h4>
+      <p>
+        Conflict of interests is very important concept in insurance companies. While in most industries companies stockholders interest is
+        aligned with providing good service to its customers in insurance industry it is not allways a case. Profit of insurance company
+        comes charging premiums higher the payouts. Therefore profitability of its business strictly depends on how many claim it will be
+        able to reject and how big premiums it will be able to market as fair. <b> And that is great space for mutuals, </b>mutuals works differently
+        they are co-owned by their customers. Therefore there is no customer-owner conflict. That extraordinary. It is hard to bring another
+        such example in economics.
+      </p>
+      <p> But it is not all that perfect. Because usually average (measured by amount of shares) mutuals owner is less aware of its rights than
+        average stock owner manager-owner conflict kicks in. Information assymetry beetwen managers and mutuals owners usuall makes problem even worse.
+        Thats <b>perfect case for blockchain</b> with natural <b>transparency</b> and <b>immutability</b>
+        of blockchain Fellows Network can build tools helping mutuals owners to supervisor managers. With <b>automated execution</b> can easily
+        build <b>votings</b> and <b>veto</b> functionality.</p>
 
     </div>
   </div>
