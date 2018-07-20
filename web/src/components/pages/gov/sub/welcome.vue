aaa17f02c67da95d639c80871241689234507754
Adam <adamskrodzki@gmail.com>
2018-07-20 12:47:24 +0200

    Token abbrevation update

--- web/src/components/pages/gov/sub/welcome.vue
+++ web/src/components/pages/gov/sub/welcome.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="content-box">
     <div class="desc">
-      <h5>What is FCT and how it will help build the platform</h5>
-      <p>FCT is a token with total supply of 10 000 000 units, with initial price of 1 POA per token.
-         FCT token gives privilege of voting on further platform development and funds distribution.
-         FCT tokens can be also gained by contribution to the development of a platform (see Join Us tab).
-        <BR/>To buy FCT token and gain part of control over the project see Governance -> Buy Tokens.
+      <h5>What is FLS and how it will help build the platform</h5>
+      <p>FLS is a token with total supply of 20 000 000 units, with initial price of 1 POA per token.
+         FLS token gives privilege of voting on further platform development and funds distribution.
+         FLS tokens can be also gained by contribution to the development of a platform (see Join Us tab).
+        <BR/>To buy FLS token and gain part of control over the project see Governance -> Buy Tokens.
         <BR/><B>All POA aquired during token sale goes to Development Fund Smart Contract.</B> </p>
       <p> Initial distribution of Tokens is as follows</p>
       <ul>
-        <li>100 000 tokens (1%) is in hands of platform creator</li>
-        <li>9 900 000 (99,0%) is in hands of Development Fund smart contract </li>
+        <li>200 000 tokens (1%) is in hands of platform creator</li>
+        <li>9 800 000 (49,0%) is in hands of Development Fund smart contract </li>
+        <li>10 000 000 (50%) is in company operation smart contract</li>
 
       </ul>
       <p>
-        To request money (POA or FCT) from Development Fund Smart Contract go to "<a href="#/gov/funding">Governance>Funding</a>".It is highly adviced to start discussion about your reward <a href="https://github.com/FellowChain/Administration" >here</a> first
+        funds in company operation smart contract will be used to pay for currency stabilisation services
+        and for mutuals liquidity and capital providers mechanism (see more in <a href="#/prod">FUTURE PRODUCTS</a>)
+      </p>
+      <p>
+        To request money (POA or FLS) from Development Fund Smart Contract go to "<a href="#/gov/funding">Governance>Funding</a>".It is highly adviced to start discussion about your reward <a href="https://github.com/FellowChain/Administration" >here</a> first
       </p>
     </div>
   </div>
